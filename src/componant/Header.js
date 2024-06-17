3d74ed725c11e9251fcb779f654a69aad9f980b5
MADUSHKA <kamadushkachathuranga@gmail.com>
2024-06-17 14:22:52 +0530

    update header

--- src/componant/Header.js
+++ src/componant/Header.js
@@ -12,7 +12,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 import LOGO from "../Image/LOGO11.jpg";
 
 const pages = [
@@ -45,12 +45,16 @@ export default function Header() {
 
   return (
     // <AppBar position="static" sx={{backgroundColor:"rgb(18, 104, 18)"}}>
-       <AppBar position="static" sx={{backgroundColor:"rgb(0, 0, 0)"}}>
+    <AppBar position="static" sx={{ backgroundColor: "rgb(0, 0, 0)" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/* <AdbIcon sx={{ display: { xs: "none", lg: "flex" }, mr: 1 }} /> */}
           {/* <BeachAccessIcon sx={{ display: { xs: "none", lg: "flex" }, mr: 1 }} /> */}
-          <Avatar alt="Remy Sharp" src={LOGO}  sx={{ display: { xs: "none", lg: "flex" }}}/>
+          <Avatar
+            alt="Remy Sharp"
+            src={LOGO}
+            sx={{ display: { xs: "none", lg: "flex" } }}
+          />&nbsp;
           <Typography
             variant="h6"
             noWrap
@@ -60,13 +64,14 @@ export default function Header() {
               mr: 2,
               display: { xs: "none", lg: "flex" },
               fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
+              fontWeight: 600,
+              letterSpacing: ".2rem",
               color: "#be43fb",
               textDecoration: "none",
             }}
           >
-            &nbsp; Mirissa Info
+           
+             Mirissa Info
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", lg: "none" } }}>
@@ -76,9 +81,9 @@ export default function Header() {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color= "#be43fb"
+              color="#be43fb"
             >
-              <MenuIcon sx={{color:"#be43fb"}} />
+              <MenuIcon sx={{ color: "#be43fb" }} />
             </IconButton>
             <Menu
               id="menu-appbar"
@@ -102,7 +107,7 @@ export default function Header() {
                 <MenuItem
                   key={page}
                   onClick={() => {
-                    handleCloseNavMenu(); 
+                    handleCloseNavMenu();
                     if (page === "Tours & Hires") {
                       window.location.href = "/ToursAndHires"; // Navigate to "/Plans_and_Diets" for the "Blog" button
                     }
@@ -119,9 +124,19 @@ export default function Header() {
                       window.location.href = "/Channel_Nutritionist"; // Navigate to "/Plans_and_Diets" for the "Blog" button
                     }
                   }}
-                  sx={{backgroundColor:"rgb(0, 0, 0)"}}
+                  sx={{ backgroundColor: "rgb(0, 0, 0)" }}
                 >
-                  <Typography textAlign="center" sx={{color:"#be43fb"}}>{page}</Typography>
+                  <Typography
+                    textAlign="center"
+                    sx={{
+                      color: "#be43fb",
+                      // "&:hover": {
+                      //   Color: "rgba(0,0,0)", // Change this to your desired hover color
+                      // }
+                    }}
+                  >
+                    {page}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -169,51 +184,39 @@ export default function Header() {
                     window.location.href = "/Channel_Nutritionist"; // Navigate to "/Plans_and_Diets" for the "Blog" button
                   }
                 }}
-                sx={{ my: 2, color: "#be43fb", display: "block", }}
+                sx={{
+                  my: 2,
+                  color: "#be43fb",
+                  display: "block",
+                  "&:hover": {
+                    backgroundColor: "#be43fb",
+                    color: "black",
+                    fontWeight:"bold"
+                  },
+                }}
               >
                 {page}
               </Button>
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            {/* <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg&ga=GA1.1.972764281.1692088791&semt=ais" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} 
-                onClick={() => {
-                  if (setting === "LogIn") {
-                    window.location.href = "/Login"; 
-                  }
-                  if (setting === "Profile") {
-                    window.location.href = "/Profile"; 
-                  }
-                }}
-                >
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-          </Box>
+          <Box sx={{ display: { xs: "none", lg: "flex" },}}>
+        <Typography
+          variant="body1"
+          sx={{
+            color: "#be43fb",
+            fontFamily: "monospace",
+            fontWeight: 700,
+            // backgroundColor: "rgba(255, 255, 255, 0.1)", // Background color with some transparency
+            padding: "5px 10px", // Padding inside the box
+            border: "1px solid #be43fb", // Border color and width
+            borderRadius: "5px", // Rounded corners
+            
+          }}
+        >
+          Contact:+9477 123 1212
+        </Typography>
+      </Box>
         </Toolbar>
       </Container>
     </AppBar>
