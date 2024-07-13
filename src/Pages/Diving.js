7950601d54d9de689a3ffc86891b4d8d8bf92a44
MADUSHKA <kamadushkachathuranga@gmail.com>
2024-07-13 17:53:16 +0530

    update the card

--- src/Pages/Diving.js
+++ src/Pages/Diving.js
@@ -1,244 +1,139 @@
-import { Key } from '@mui/icons-material'
-import { Box, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
-import React from 'react'
-import H1 from "../Image/H1.webp";
-import H2 from "../Image/H2.png";
-import H3 from "../Image/H3.png";
-import H4 from "../Image/H4.png";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import imgS1 from "../Image/Diving.jpg";
+import { red } from "@mui/material/colors";
 
-
+const CardData = [
+  {
+    id: 1,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/ToursAndHires",
+  },
+  {
+    id: 2,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/Diving",
+  },
+  {
+    id: 3,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/WhaleWatching",
+  },
+  {
+    id: 4,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/VehicleRent",
+  },
+  {
+    id: 5,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/MobileTopup",
+  },
+  {
+    id: 6,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/Restaurants",
+  },
+  {
+    id: 7,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/MassagingAndSpa",
+  },
+  {
+    id: 8,
+    image: imgS1,
+    cardName: "Mirissa infor Diving card",
+    cardSubName: "Mirissa best business site",
+    // path: "/Surfing",
+  },
+];
 export default function Diving() {
+  const sellingCard = (item, key) => (
+    <Grid key={key} item xs={12} sm={6} md={4} lg={4} xl={3}>
+      {/* <Box
+          sx={{
+            Width:'auto',
+            height: 300,
+            // backgroundColor: "transparent",
+            // borderRadius: 6,
+            backgroundColor: "rgba(197, 235, 170, 0.5)",
+            border: "transparent",
+          }}
+        > */}
+      <Card
+        sx={{ maxWidth: 485, alignItems: "center", justifyContent: "center", paddingLeft:3, paddingRight:3 }}
+      >
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          // action={
+          //   <IconButton aria-label="settings">
+          //     <MoreVertIcon />
+          //   </IconButton>
+          // }
+          title={item.cardName}
+          subheader={item.cardSubName}
+        />
+        <CardMedia
+          component="img"
+          height="280"
+          image={imgS1}
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+      </Card>
+      {/* </Box> */}
+    </Grid>
+  );
+
+
   return (
     <>
-      <Grid container spacing={2} marginTop={3} marginBottom={3}>
-          <Grid key={Key} item xs={12} sm={12} md={6} lg={6} xl={6}>
-            <Card
-              sx={{
-                display: "flex",
-                Width: 345,
-                height: 390,
-                borderRadius: 6,
-                boxShadow: "0px 3px 6px rgba(0, 0, 0, 0)",
-                background:"rgb(60, 179, 113, 0.1)",
-              }}
-            >
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <CardContent sx={{ flex: "1 0 auto" }}>
-                  <Box
-                    sx={{
-                      backgroundColor: "rgb(60, 179, 113, 0.3)",
-
-                      padding: 1.5,
-                      // paddingBottom:4,
-                      borderRadius: 8,
-                    }}
-                  >
-                    <Typography component="div" variant="h5">
-                      WEIGHT LOSS
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      Forget starving yourself to lose weight. Let us introduce
-                      you to a healthy regime where you make smart food choices
-                      without having to skip them to lose weight.
-                    </Typography>
-                  </Box>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      height: 235,
-                      width: 240,
-                      display: { xs: "flex", sm: "none" },
-                      marginTop: 1,
-                      marginLeft: 11,
-                    }}
-                    image={H1}
-                    // alt="Live from space album cover"
-                  />
-                </CardContent>
-              </Box>
-              <CardMedia
-                component="img"
-                sx={{
-                  height: 235,
-                  width: 200,
-                  display: { xs: "none", sm: "flex" },
-                }}
-                image={H1}
-                // alt="Live from space album cover"
-              />
-            </Card>
-          </Grid>
-          <Grid key={Key} item xs={12} sm={12} md={6} lg={6} xl={6}>
-            <Card
-              sx={{
-                display: "flex",
-                Width: 345,
-                height: 420,
-                borderRadius: 6,
-                boxShadow: "0px 3px 6px rgba(0, 0, 0, 0)",
-              }}
-            >
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <CardContent sx={{ flex: "1 0 auto" }}>
-                  <Box
-                    sx={{
-                      backgroundColor: "rgba(255, 99, 71, 0.3)",
-
-                      padding: 1.5,
-                      // paddingBottom:4,
-                      borderRadius: 8,
-                    }}
-                  >
-                    <Typography component="div" variant="h5">
-                      ATHLETE
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      When you are an athlete, you need a meal plan that will
-                      support your rigorous activities, which is exactly what we
-                      serve.
-                    </Typography>
-                  </Box>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      width: 340,
-                      display: { xs: "flex", sm: "none" },
-                      marginTop: 1,
-                      marginLeft: 9,
-                    }}
-                    image={H2}
-                    // alt="Live from space album cover"
-                  />
-                </CardContent>
-              </Box>
-              <CardMedia
-                component="img"
-                sx={{ width: 240, display: { xs: "none", sm: "flex" } }}
-                image={H2}
-                // alt="Live from space album cover"
-              />
-            </Card>
-          </Grid>
-          <Grid key={Key} item xs={12} sm={12} md={6} lg={6} xl={6}>
-            <Card
-              sx={{
-                display: "flex",
-                Width: 345,
-                height: 400,
-                borderRadius: 6,
-                boxShadow: "0px 3px 6px rgba(0, 0, 0, 0)",
-              }}
-            >
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <CardContent sx={{ flex: "1 0 auto" }}>
-                  <Box
-                    sx={{
-                      backgroundColor: "rgb(106, 90, 205, 0.3)",
-
-                      padding: 1.5,
-                      // paddingBottom:4,
-                      borderRadius: 8,
-                    }}
-                  >
-                    <Typography component="div" variant="h5">
-                      WELLNESS
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      Lead a wholesome life with our wellness meal plan that
-                      will enhance the quality of your life, keeping you healthy
-                      across the board.
-                    </Typography>
-                  </Box>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      width: 332,
-                      display: { xs: "flex", sm: "none" },
-                      marginTop: 1,
-                      marginLeft: 11,
-                    }}
-                    image={H3}
-                    // alt="Live from space album cover"
-                  />
-                </CardContent>
-              </Box>
-              <CardMedia
-                component="img"
-                sx={{ width: 230, display: { xs: "none", sm: "flex" } }}
-                image={H3}
-                // alt="Live from space album cover"
-              />
-            </Card>
-          </Grid>
-          <Grid key={Key} item xs={12} sm={12} md={6} lg={6} xl={6}>
-            <Card
-              sx={{
-                display: "flex",
-                Width: 345,
-                height: 400,
-                borderRadius: 6,
-                boxShadow: "0px 3px 6px rgba(0, 0, 0, 0)",
-              }}
-            >
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <CardContent sx={{ flex: "1 0 auto" }}>
-                  <Box
-                    sx={{
-                      backgroundColor: "rgb(255, 152, 10, 0.3)",
-
-                      padding: 1.5,
-                      // paddingBottom:4,
-                      borderRadius: 8,
-                    }}
-                  >
-                    <Typography component="div" variant="h5">
-                      MUSCLE GAIN
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      Give your muscles the necessary boost with our
-                      muscle-enhancing food through our meal plan which will
-                      accelerate your fitness journey with remarkable positive
-                      results.
-                    </Typography>
-                  </Box>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      marginTop: 1,
-                      width: 332,
-                      display: { xs: "flex", sm: "none" },
-                      marginLeft: 10,
-                    }}
-                    image={H4}
-                    // alt="Live from space album cover"
-                  />
-                </CardContent>
-              </Box>
-              <CardMedia
-                component="img"
-                sx={{ width: 175, display: { xs: "none", sm: "flex" } }}
-                image={H4}
-                // alt="Live from space album cover"
-              />
-            </Card>
-          </Grid>
+      <Container>
+        <Grid container spacing={2} marginTop={3} marginBottom={3}>
+          {CardData.map((card, key) => sellingCard(card, key))}
         </Grid>
+      </Container>
     </>
-  )
+      
+  );
 }
--- src/Pages/VehicleRent.js
+++ src/Pages/VehicleRent.js
@@ -1,7 +1,137 @@
-import React from 'react'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import imgS1 from "../Image/Vehicle_Rent.jpg";
+import { Box, Container, Grid } from "@mui/material";
+import { Key } from "@mui/icons-material";
+
+const CardData = [
+  {
+    id: 1,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/ToursAndHires",
+  },
+  {
+    id: 2,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/Diving",
+  },
+  {
+    id: 3,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/WhaleWatching",
+  },
+  {
+    id: 4,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/VehicleRent",
+  },
+  {
+    id: 5,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/MobileTopup",
+  },
+  {
+    id: 6,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/Restaurants",
+  },
+  {
+    id: 7,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/MassagingAndSpa",
+  },
+  {
+    id: 8,
+    image: imgS1,
+    cardName: "Mirissa infor Vehicle Rent card",
+    cardSubName: "Mirissa best business site",
+    // path: "/Surfing",
+  },
+];
 
 export default function VehicleRent() {
+  const sellingCard = (item, key) => (
+    <Grid key={key} item xs={12} sm={6} md={4} lg={4} xl={3}>
+      {/* <Box
+          sx={{
+            Width:'auto',
+            height: 300,
+            // backgroundColor: "transparent",
+            // borderRadius: 6,
+            backgroundColor: "rgba(197, 235, 170, 0.5)",
+            border: "transparent",
+          }}
+        > */}
+      <Card
+        sx={{ maxWidth: 485, alignItems: "center", justifyContent: "center", paddingLeft:3, paddingRight:3 }}
+      >
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          // action={
+          //   <IconButton aria-label="settings">
+          //     <MoreVertIcon />
+          //   </IconButton>
+          // }
+          title={item.cardName}
+          subheader={item.cardSubName}
+        />
+        <CardMedia
+          component="img"
+          height="280"
+          image={imgS1}
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+      </Card>
+      {/* </Box> */}
+    </Grid>
+  );
+
   return (
-    <div>VehicleRent</div>
-  )
+    <>
+      <Container>
+        <Grid container spacing={2} marginTop={3} marginBottom={3}>
+          {CardData.map((card, key) => sellingCard(card, key))}
+        </Grid>
+      </Container>
+    </>
+  );
 }
